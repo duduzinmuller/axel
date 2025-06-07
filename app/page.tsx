bb5393eed883c3d1e05c69f977d27b008a1e9f69
Eduardo <eduardomuller989@gmail.com>
2025-06-07 10:06:07 -0400

    chore: add husky setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-"use client";
-import { useState } from "react";
-
 export default function Home() {
-  const [clicked, setClicked] = useState("");
-  function Clicou() {
-    setClicked("Clicou");
-    console.log(clicked);
-  }
   return (
-    <div className="bg-black flex flex-col justify-center items-center">
-      <h1>Hello World</h1>
-      <button onClick={Clicou} className="bg-red-600">
-        Clicou
-      </button>
+    <div className="flex items-center bg-black">
+      <h1>Ola</h1>
     </div>
   );
 }
