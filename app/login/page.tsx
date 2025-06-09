187f732c6025343344bf80f4dfc99ae3512c364d
Eduardo <eduardomuller989@gmail.com>
2025-06-09 08:40:54 -0400

    feat: add LoginPage component and integrate react-icons for social login options

---
+++ app/login/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Mail, Lock } from "lucide-react";
+import Image from "next/image";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+
+export default function LoginPage() {
+  return (
+    <main className="relative flex min-h-screen items-center justify-center">
+      <Card className="w-[380px] border border-zinc-800 bg-transparent text-center">
+        <CardContent className="px-6 py-8">
+          <div className="mb-4">
+            <Image
+              width={80}
+              src="/axel.svg"
+              className="mx-auto"
+              height={80}
+              alt={""}
+            />
+          </div>
+          <h2 className="mb-2 text-2xl font-medium">Bem-vindo de volta</h2>
+          <p className="mb-5 text-sm text-zinc-400">
+            Ainda não possuí uma conta?{" "}
+            <a href="#" className="text-blue-500 hover:underline">
+              Criar conta
+            </a>
+          </p>
+
+          <form className="space-y-4">
+            <div className="relative">
+              <Mail
+                className="absolute top-1/2 left-3 -translate-y-1/2 text-zinc-400"
+                size={16}
+              />
+              <Input
+                type="email"
+                placeholder="Email address"
+                className="border-zinc-700 bg-zinc-800 pl-10 text-white"
+                required
+              />
+            </div>
+            <div className="relative">
+              <Lock
+                className="absolute top-1/2 left-3 -translate-y-1/2 text-zinc-400"
+                size={16}
+              />
+              <Input
+                type="password"
+                placeholder="Password"
+                className="border-zinc-700 bg-zinc-800 pl-10 text-white"
+                required
+              />
+            </div>
+            <Button className="bg-gradient-axel w-full cursor-pointer transition-all duration-300 hover:scale-105 hover:opacity-90">
+              Login
+            </Button>
+          </form>
+
+          <div className="my-5 flex items-center">
+            <div className="flex-1 border-t border-zinc-700" />
+            <span className="mx-3 text-sm text-zinc-500">OU</span>
+            <div className="flex-1 border-t border-zinc-700" />
+          </div>
+
+          <div className="flex justify-center gap-4">
+            <Button
+              variant="outline"
+              className="h-12 w-20 cursor-pointer border-zinc-700"
+            >
+              <FcGoogle size={24} />
+            </Button>
+            <Button
+              variant="outline"
+              className="h-12 w-20 cursor-pointer border-zinc-700"
+            >
+              <FaGithub size={24} />
+            </Button>
+            <Button
+              variant="outline"
+              className="h-12 w-20 cursor-pointer border-zinc-700"
+            >
+              <FaTwitter size={24} />
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
