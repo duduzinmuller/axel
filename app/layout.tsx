fbb3b22223dd0e5ebe42cabbe938515afd437e04
Eduardo <eduardomuller989@gmail.com>
2025-06-10 11:28:51 -0400

    feat: add axios and sonner dependencies, implement Toaster component for notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <ClientOnly>
           <ThemeProvider>{children}</ThemeProvider>
         </ClientOnly>
