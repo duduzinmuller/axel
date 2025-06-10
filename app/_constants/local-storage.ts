ab7debd840a25fda935398102a3a93df6050b880
Eduardo <eduardomuller989@gmail.com>
2025-06-10 12:16:43 -0400

    refactor: rename USER constant to LOCAL_STORAGE_USER_KEY for clarity

--- app/_constants/local-storage.ts
+++ app/_constants/local-storage.ts
@@ -1,3 +1,3 @@
 export const LOCAL_STORAGE_ACCESS_TOKEN_KEY = "accessToken";
 export const LOCAL_STORAGE_REFRESH_TOKEN_KEY = "refreshToken";
-export const USER = "user";
+export const LOCAL_STORAGE_USER_KEY = "user";
