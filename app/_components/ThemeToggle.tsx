6d4e2fc111b50c1794b23061766d610331eec6ae
Aldair Rocha <aldairmurilo000@gmail.com>
2025-06-18 16:19:30 -0300

    feat: adicionar página de perfil com funcionalidade de edição de perfil e seleção de avatar

--- app/_components/ThemeToggle.tsx
+++ app/_components/ThemeToggle.tsx
@@ -12,7 +12,7 @@ const ThemeToggle = () => {
       variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="h-10 w-10"
+      className="h-8 w-8"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
