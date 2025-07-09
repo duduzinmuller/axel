c30d147496bf4da623d7885790e233d8b53578c2
duduzinmuller <eduardomuller989@gmail.com>
2025-07-09 17:21:56 -0400

    feat: add user status service and hook for managing user status state

--- app/_lib/hooks/index.ts
+++ app/_lib/hooks/index.ts
@@ -2,3 +2,4 @@ export { useAI } from "./useAI";
 export { useMessageLimit } from "./useMessageLimit";
 export { useUsage } from "./useUsage";
 export * from "./useMessageUsage";
+export { useUserStatus } from "./useUserStatus";
